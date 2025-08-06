65c82a5cc088e43014bcc71ee1af05deb5456dbc
Weslei Possidonio <weslei.possidonio@gmail.com>
2025-08-06 04:07:09 -0300

    add new sections

---
+++ src/pages/Home/components/CallAction/index.tsx
@@ -0,0 +1,42 @@
+
+import image from '@/assets/bg-bannerOne.jpg'
+import { Button } from '@/components/ui/button'
+
+
+export const CallAction = () => {
+  return (
+    <section
+      className="relative w-full h-auto flex items-center overflow-hidden"
+      style={{
+        minHeight: '35rem',
+        backgroundImage: `url(${image})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundAttachment: 'fixed',
+      }}
+    >
+      {/* Overlay branca sutil */}
+      <div className="absolute inset-0 z-0 pointer-events-none" style={{
+        backgroundColor: 'rgba(0, 0, 0, 0.4)', // branco com 10% de opacidade
+      }} />
+
+      <div className="container mx-auto py-10 px-8 space-y-5 flex flex-col items-start justify-center gap-7 relative z-5">
+        <div className="w-full flex flex-col items-center justify-start gap-5" data-aos="fade-right" data-aos-duration="1000">
+          <div className="self-center text-center">
+            <h1 className="text-white text-2xl md:text-4xl font-semibold drop-shadow-md">
+              Design, elegância e exclusividade <br /> em cada detalhe.
+            </h1>
+
+            <p className="text-white text-md md:text-lg m-0 self-start drop-shadow-md mt-5">
+              Soluções sob medida que unem estética refinada e performance. Um novo padrão de acabamento para quem exige o melhor.
+            </p>
+
+            <Button className="w-max py-7 mt-4 text-lg bg-base-blue border border-base-blue text-white font-semibold">
+              Entrar em Contato agora!
+            </Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
---
+++ src/pages/Home/components/Footer/index.tsx
@@ -0,0 +1,25 @@
+import ImageLogo from '@/assets/Logo2.png'
+export const Footer = () => {
+  return (
+    <footer className="w-full h-[20rem] bg-neutral-5git0 mt-16">
+      <div className="container mx-auto flex items-start justify-between px-8 py-16">
+        <div className='space-y-2'>
+          <img src={ImageLogo} className='w-35' alt="alves-glass-logo" />
+          <p className='text-neutral-700 text-sm'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br /> Assumenda, saepe. Ad veniam, magnam culpa
+          </p>
+        </div>
+
+        <div className='space-x-5'>
+          <a href="/">Home</a>
+          <a href="#about">Sobre Nós</a>
+          <a href="#services">Serviços</a>
+          <a href="#portfolio">Portfólio</a>
+          <a href="#contact">Contatos</a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+
